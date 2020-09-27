7c7da6d458c9e726a67d9522494fbc3278216846
Mattias Lasersköld <mattias.l.sk@gmail.com>
2020-09-27 16:50:28 +0200

    Moved test code into testing framework

--- test/localcompiler_test.cpp
+++ test/localcompiler_test.cpp
@@ -2,18 +2,18 @@
 #import "localcompiler.h"
 #import "mls-unit-test/unittest.h"
 #import "mocks/mockenvironment.h"
+#import <memory>
 
 TEST_SUIT_BEGIN
 
-TEST_CASE("print something") {
+TEST_CASE("should pass arguments to environment") {
     auto mockEnvironment = std::make_shared<MockEnvironment>();
     auto compiler = createLocalCompiler("g++", mockEnvironment);
 
-    mockEnvironment->runMock.expectCall(1);
+    mockEnvironment->mock_run.expectNum(1).expectArgs(
+        "g++", {"-c", "main.cpp", "-o", "main.o"});
 
-    compiler->compile("-c main.cpp", "-o main.o");
-
-    std::cout << "hello\n";
+    compiler->compile("-c", "main.cpp", "-o", "main.o");
 }
 
 TEST_SUIT_END
--- test/mocks/mockenvironment.h
+++ test/mocks/mockenvironment.h
@@ -1,68 +1,11 @@
 #pragma once
 
 #import "ienvironment.h"
-#import <functional>
-#import <iostream>
-#import <mls-unit-test/unittestvars.h>
-#import <type_traits>
-
-#define MOCK_METHOD(ret, name, args)                                           \
-    MockedFunction<ret(args)> mocked_##name =                                  \
-        MockedFunction<ret(args)>(#name);                                      \
-    ret name##Mock(args) { /* how to solve? */                                 \
-    }
-
-template <typename T>
-class MockedFunction {};
-
-// template <typename Signature>
-template <typename ReturnT, typename... ArgsT>
-class MockedFunction<ReturnT(ArgsT...)> {
-public:
-    MockedFunction(std::string name) : name(name) {
-    }
-
-    ReturnT operator()(ArgsT... args) {
-        ++numCalls;
-        if (onCall) {
-            return onCall(args...);
-        }
-        else {
-            return {};
-        }
-    }
-
-    ~MockedFunction() {
-        if (numCalls != expectedNumCalls) {
-            std::cerr << "function " << name << " expected to be called "
-                      << expectedNumCalls << " but was called " << numCalls
-                      << " times \n";
-            unittest::failTest();
-        }
-    }
-
-    void expectCall(size_t num) {
-        expectedNumCalls = num;
-    }
-
-private:
-    int returnValue = {};
-    std::function<ReturnT(ArgsT...)> onCall;
-    size_t numCalls = 0;
-    size_t expectedNumCalls = 0;
-    std::string name;
-};
+#import <mls-unit-test/mock.h>
 
 class MockEnvironment : public IEnvironment {
 public:
-    using runType = int(std::string program,
-                        std::vector<std::string> arguments);
-
-    MockedFunction<runType> runMock = MockedFunction<runType>("run");
-
-    int run(std::string program, std::vector<std::string> arguments) override {
-        return runMock(program, arguments);
-    }
-
-    //    MOCK_METHOD(int, run, (std::string, std::vector.std::string >));
+    MOCK_METHOD2(int,
+                 run,
+                 (std::string program, std::vector<std::string> arguments))
 };
